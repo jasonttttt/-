2973585ddaa991416f87f380e162e5914039b092
唐鑫 <v-tangxin@xiaoniu66.com>
2018-08-07 18:44:50 +0800

    两个人所有的Api

--- api/apis/test/getProdcution.js
+++ api/apis/test/getProdcution.js
@@ -6,14 +6,16 @@ module.exports = {
 				yield: "6.3-7.2%",
 				time: "03/05-04/12",
 				raisingTime: "募集时间",
-				term: "24"
+				term: "24",
+				i:50
 			},
 			{
 				company: "长春长生公司(一期)",
 				yield: "6.3-7.2%",
 				time: "03/05-04/12",
 				raisingTime: "募集时间",
-				term: "24"
+				term: "24",
+				i:100
 			},
 		
 	],
@@ -23,7 +25,16 @@ module.exports = {
 			yield: "6.3-7.2%",
 			time: "03/05-04/12",
 			raisingTime: "募集时间",
-			term: "24"
-		}
+			term: "24",
+			i:20
+		},
+		{
+			company: "武汉生物(二期)",
+			yield: "6.3-7.2%",
+			time: "03/05-04/12",
+			raisingTime: "募集时间",
+			term: "24",
+			i:80
+		},
 	],
 }
