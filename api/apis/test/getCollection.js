b90302ac9ff880d371732f7f4261ab8696751948
唐鑫 <v-tangxin@xiaoniu66.com>
2018-08-09 10:12:21 +0800

    数据格式有误 修改之

--- api/apis/test/getCollection.js
+++ api/apis/test/getCollection.js
@@ -2,14 +2,14 @@ module.exports = {
 	state:1,
 	list: [
 		{
-			company: "长春长生公司(一期))",
+			company: "长春长生公司(一期)",
 			yield: "6.3-7.2%",
 			time: "03/05-04/12",
 			raisingTime: "募集时间",
 			term: "24"
 		},
 		{
-			company: "长春长生公司(一期))",
+			company: "长春长生公司(一期)",
 			yield: "6.3-7.2%",
 			time: "03/05-04/12",
 			raisingTime: "募集时间",
