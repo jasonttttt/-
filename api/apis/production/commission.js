b6f8bbc967b0866943d52ecf48a6aeff52c9a800
唐鑫 <v-tangxin@xiaoniu66.com>
2018-08-10 18:36:48 +0800

    加上帅哥的 mock

---
+++ api/apis/production/commission.js
@@ -0,0 +1,43 @@
+module.exports = {
+  state:1,
+  data : [
+    {
+      name:'在售产品3',
+      list:[
+        {
+          title:'新疆前海联合海盈货币 A(一期)',
+          yield:'6.3-7.2%',
+          date: '03/05-04/12',
+          limit:'24',
+          percent:30,
+        },
+        {
+          title:'新疆前海联合海盈货币 B(一期)',
+          yield:'6.3-7.2%',
+          amount:1000,
+          limit:'24',
+          percent:40,
+        }
+      ],
+    },
+    {
+      name:'预售产品3',
+      list: [
+        {
+          title:'新疆前海联合海盈货币 C(一期)',
+          yield:'6.3-7.2%',
+          date :'03/05-04/12',
+          limit:'24',
+          percent:70,
+        },
+        {
+          title:'新疆前海联合海盈货币 D(一期)',
+          yield:'6.3-7.2%',
+          amount:1000,
+          limit:'24',
+          percent:100,
+        }
+      ]
+    }
+  ]
+}
