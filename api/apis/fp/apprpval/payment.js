a19bbe195b6b9bd953ad687274696dc39a356a65
唐鑫 <v-tangxin@xiaoniu66.com>
2018-08-14 11:15:46 +0800

    提交api

---
+++ api/apis/fp/apprpval/payment.js
@@ -0,0 +1,19 @@
+module.exports = {
+	state: 1,
+	data: [{
+			companyNumber: "新疆前海盈货币B(00248)",
+			expiryTime: "18/05/20",
+			paymentTime: "十天内"
+		},
+		{
+			companyNumber: "新疆前海盈货币B(00248)",
+			expiryTime: "18/05/20",
+			paymentTime: "十天内"
+		},
+		{
+			companyNumber: "新疆前海盈货币B(00248)",
+			expiryTime: "18/05/20",
+			paymentTime: "十天内"
+		}
+	]
+}
---
+++ api/apis/fp/apprpval/progress.js
@@ -0,0 +1,19 @@
+module.exports = {
+	state: 1,
+	data: [{
+			username: "张一鸣",
+			company: "新疆前海盈货币B",
+			coordinate: 1
+		},
+		{
+			username: "窝窝头",
+			company: "新疆前海盈货币B",
+			coordinate: 2
+		},
+		{
+			username: "阿尔萨斯",
+			company: "新疆前海盈货币B",
+			coordinate: 1
+		}
+	]
+}
