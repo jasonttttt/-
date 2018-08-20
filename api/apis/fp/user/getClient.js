0a45d8f00a6b02c4f458b38788dd1613cc232837
唐鑫 <v-tangxin@xiaoniu66.com>
2018-08-20 16:57:53 +0800

    更改我的客户 API

--- api/apis/fp/user/getClient.js
+++ api/apis/fp/user/getClient.js
@@ -3,22 +3,37 @@ module.exports = {
 	list: [{
 			name: "吴晓君",
 			phone: "18274994930",
-			company: "牛鼎丰"
+			company: [
+					{companyName:'新疆前海联合海盈货币Aaaaa'},
+					{companyName:'新疆前海联合海盈货币B'},
+					{companyName:'新疆前海联合海盈货币C'},
+			]
 		},
 		{
 			name: "吴晓君",
 			phone: "18274994930",
-			company: "牛鼎丰"
+			company: [
+					{companyName:'新疆前海联合海盈货币A'},
+					{companyName:'新疆前海联合海盈货币 Baaa'},
+					{companyName:'新疆前海联合海盈货币 B'},
+			]
 		},
 		{
 			name: "吴晓君",
 			phone: "18274994930",
-			company: "牛鼎丰牛鼎丰牛鼎丰牛鼎丰"
+			company: [
+					{companyName:'新疆前海联合海盈货币A'},
+					{companyName:'新疆前海联合海盈货币Bbbb'},
+					{companyName:'新疆前海联合海盈货币C'},
+					{companyName:'新疆前海联合海盈货币ccC'},
+			]
 		},
 		{
 			name: "吴晓君",
 			phone: "18274994930",
-			company: "牛鼎丰"
+			company: [
+					{companyName:'新疆前海联合海盈货币A'},
+			]
 		}
 	]
 }
