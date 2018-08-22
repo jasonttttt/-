02208012c40ff50a1a47d5594578c2b9d5435c45
唐鑫 <v-tangxin@xiaoniu66.com>
2018-08-22 18:12:49 +0800

    修改 我的客户api

--- api/apis/fp/user/boughtData.js
+++ api/apis/fp/user/boughtData.js
@@ -1,38 +1,67 @@
 module.exports = {
-    state:1,
-    data : [
-      {
-        id:1,
-        name:'未到期产品',
-        // beforeIcon:require('~/assets/img/user/report-after/new.png'),
-        // afterIcon:require('~/assets/img/user/report-after/new1.png'),
-        list:[
-          {
-            title:'新疆前海联合海盈货币B (002248)',
-            money:'800万',
-            date: '17/06/22',
-            expire:'18/04/03',
-          },
-          {
-            title:'新疆前海联合海盈货币 (002248)',
-            money:'700万',
-            date: '17/06/22',
-            expire:'18/05/20',
-          }
-        ],
-      },
-      {
-        id:2,
-        name:'已到期产品',
-        
-        list: [
-          {
-            title:'新疆前海联合海盈货币B (002248)',
-            money:'500万',
-            date: '16/06/22',
-            expire:'17/05/20',
-          }
-        ]
-      }
-    ]
+  state: 1,
+  data: [
+    {
+    user: '张一鸣',
+    tel: '72878270180',
+    undue: {
+      id: 1,
+      name: '未到期产品',
+      list: [{
+          title: '新疆前海联合海盈货币B (002248)',
+          money: '800万',
+          date: '17/06/22',
+          expire: '18/04/03',
+        },
+        {
+          title: '新疆前海联合海盈货币 (002241)',
+          money: '700万',
+          date: '17/06/22',
+          expire: '18/05/20',
+        },
+      ],
+    },
+    expire: {
+      id: 2,
+      name: '已到期产品',
+      list: [{
+        title: '新疆前海联合海盈货币B (002248)',
+        money: '500万',
+        date: '16/06/22',
+        expire: '17/05/20',
+      }]
+    }
+  },
+  {
+    user: '张一鸣',
+    tel: '72878270180',
+    undue: {
+      id: 1,
+      name: '未到期产品',
+      list: [{
+          title: '新疆前海联合海盈货币B (002248)',
+          money: '800万',
+          date: '17/06/22',
+          expire: '18/04/03',
+        },
+        {
+          title: '新疆前海联合海盈货币 (002241)',
+          money: '700万',
+          date: '17/06/22',
+          expire: '18/05/20',
+        },
+      ],
+    },
+    expire: {
+      id: 2,
+      name: '已到期产品',
+      list: [{
+        title: '新疆前海联合海盈货币B (002248)',
+        money: '500万',
+        date: '16/06/22',
+        expire: '17/05/20',
+      }]
+    }
   }
+]
+}
