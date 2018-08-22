e6f7fb1839ad0b800c1586df1f01a681ff4ce650
唐鑫 <v-tangxin@xiaoniu66.com>
2018-08-22 15:52:17 +0800

    我的客户 api

--- api/apis/fp/user/getClient.js
+++ api/apis/fp/user/getClient.js
@@ -3,36 +3,46 @@ module.exports = {
 	list: [{
 			name: "吴晓君",
 			phone: "18274994930",
-			company: [
-					{companyName:'新疆前海联合海盈货币Aaaaa'},
-					{companyName:'新疆前海联合海盈货币B'},
-					{companyName:'新疆前海联合海盈货币C'},
+			product: [
+					{title:'新疆前海联合海盈货币A',money:'800万', date: '17/06/22',expire:'18/04/03'},
+					{title:'新疆前海联合海盈货币B',money:'700万', date: '17/06/02',expire:'18/04/03'},
+					{title:'新疆前海联合海盈货币C',money:'500万', date: '17/06/11',expire:'18/02/03'},
+			],
+			expireProduct:[
+					{companyName:'武汉生物研究院A',money:'600万', date: '17/06/11',expire:'18/02/03'}
 			]
 		},
 		{
-			name: "吴晓君",
-			phone: "18274994930",
-			company: [
-					{companyName:'新疆前海联合海盈货币A'},
-					{companyName:'新疆前海联合海盈货币 Baaa'},
-					{companyName:'新疆前海联合海盈货币 B'},
+			name: "杨小花",
+			phone: "13233585874",
+			product: [
+					{title:'新疆前海联合海盈货币A',money:'800万', date: '17/06/22',expire:'18/04/03'},
+			],
+			expireProduct:[
+					{companyName:'武汉生物研究院A',money:'600万', date: '17/06/11',expire:'18/02/03'}
 			]
 		},
 		{
-			name: "吴晓君",
-			phone: "18274994930",
-			company: [
-					{companyName:'新疆前海联合海盈货币A'},
-					{companyName:'新疆前海联合海盈货币Bbbb'},
-					{companyName:'新疆前海联合海盈货币C'},
-					{companyName:'新疆前海联合海盈货币ccC'},
+			name: "张一鸣",
+			phone: "14789632546",
+			product: [
+					{title:'新疆前海联合海盈货币A',money:'800万', date: '17/06/22',expire:'18/04/03'},
+					{title:'新疆前海联合海盈货币B',money:'700万', date: '17/06/02',expire:'18/04/03'},
+					{title:'新疆前海联合海盈货币C',money:'500万', date: '17/06/11',expire:'18/02/03'},
+			],
+			expireProduct:[
+					{companyName:'武汉生物研究院A',money:'600万', date: '17/06/11',expire:'18/02/03'}
 			]
 		},
 		{
-			name: "吴晓君",
-			phone: "18274994930",
-			company: [
-					{companyName:'新疆前海联合海盈货币A'},
+			name: "彭唐",
+			phone: "15647849854",
+			product: [
+					{title:'新疆前海联合海盈货币A',money:'800万', date: '17/06/22',expire:'18/04/03'},
+					{title:'新疆前海联合海盈货币B',money:'700万', date: '17/06/02',expire:'18/04/03'},
+			],
+			expireProduct:[
+					{companyName:'武汉生物研究院A',money:'600万', date: '17/06/11',expire:'18/02/03'}
 			]
 		}
 	]
